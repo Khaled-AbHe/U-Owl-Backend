ee34cf264cde22ef30aad2f1fee4342b83352627
Emile <emiletardificloud@icloud.com>
2026-03-31 21:46:20 -0400

    forbidWhitelisted sans le mettre en commentaire

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   }));
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
-    // forbidNonWhitelisted : true,
+    forbidNonWhitelisted : true,
     transform : true,
   }));
   await app.listen(process.env.PORT ?? 3000);
