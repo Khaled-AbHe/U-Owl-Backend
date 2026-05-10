f68debee91f3fd1867a06463a3998f7d50a9e512
Khaled-AbHe <6320936@cegepmv.ca>
2026-05-09 20:50:31 -0400

    edited vehicle controller perms a bit

--- src/vehicles/vehicles.controller.ts
+++ src/vehicles/vehicles.controller.ts
@@ -1,15 +1,23 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateVehicleDto } from './dtos/create-vehicule.dto';
 import { UpdateVehicleDto } from './dtos/update-vehicule.dto';
 import { VehiclesService } from './services/vehicles.service';
 import { AdminGuard } from '../currentUser/guards/admin.guard';
-import { AuthGuard } from '../currentUser/guards/auth.guard';
 
 @Controller('vehicles')
-@UseGuards(AuthGuard, AdminGuard)
 export class VehiclesController {
   constructor(private vehiclesService: VehiclesService) {}
 
+  @UseGuards(AdminGuard)
   @Post('/create')
   createVehicle(@Body() body: CreateVehicleDto) {
     return this.vehiclesService.factoryCreate(body);
@@ -36,18 +44,24 @@ export class VehiclesController {
     return this.vehiclesService.findVehicleById(id);
   }
 
+  @UseGuards(AdminGuard)
   @Get('/:id/roadSafe')
   isRoadSafe(@Param('id') id: number) {
     return this.vehiclesService.isRoadSafe(id);
   }
 
+  @UseGuards(AdminGuard)
   @Delete('/:id')
   deleteVehicleById(@Param('id') vehicleId: number) {
     return this.vehiclesService.deleteVehicleById(vehicleId);
   }
 
+  @UseGuards(AdminGuard)
   @Patch('/:id')
-  updateVehicle(@Param('id') vehicleId: number, @Body() body: UpdateVehicleDto) {
+  updateVehicle(
+    @Param('id') vehicleId: number,
+    @Body() body: UpdateVehicleDto,
+  ) {
     return this.vehiclesService.updateVehicle(vehicleId, body);
   }
 }
